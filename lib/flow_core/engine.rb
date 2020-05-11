8e2fa0f3af9e7a84b6966809866b6666a030ea13
zhanglinjie412@gmail.com <zhanglinjie412@gmail.com>
2020-05-11 18:45:56 +0800

    fix migration

--- lib/flow_core/engine.rb
+++ lib/flow_core/engine.rb
@@ -7,5 +7,13 @@ module FlowCore
         I18n.load_path << File.expand_path("locale/en.yml", __dir__)
       end
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
